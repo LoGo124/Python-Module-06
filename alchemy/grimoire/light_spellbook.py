de2043bafde0431306aef47bd3cf8bb1ba4c7dc7
LoGo124 <nacholopezgomez2002@gmail.com>
2026-08-27 19:59:25 +0200

    part4 a medias

---
+++ alchemy/grimoire/light_spellbook.py
@@ -0,0 +1,9 @@
+from typing import List
+
+
+def light_spell_allowed_ingredients() -> List[str]:
+    return ["earth", "air", "fire", "water"]
+
+
+def light_spell_record(spell_name: str, ingredients: str) -> str:
+    
